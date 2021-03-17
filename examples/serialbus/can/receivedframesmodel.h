94722e4347a210d8f2c03c097bee7932a42c5625
Evgeny Shtanov <shtanov_evgenii@mail.ru>
2021-03-18 01:33:06 +0300

    CAN Example: change the access in ReceivedFramesModel
    Signed-off-by: Evgeny Shtanov <shtanov_evgenii@mail.ru>
    Change-Id: Ie14f092a9768272ce3850b444cf4885d7c4dff57

--- examples/serialbus/can/receivedframesmodel.h
+++ examples/serialbus/can/receivedframesmodel.h
@@ -67,6 +67,8 @@ public:
     void appendFramesRingBuffer(const QList<QStringList> &slvector);
     void appendFramesUnlimited(const QList<QStringList> &slvector);
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     void clear();
     void setQueueLimit(int limit = 0); // 0 - unlimited
@@ -75,8 +77,6 @@ public:
     void update();
 
 protected:
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
     bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
 
 private:
